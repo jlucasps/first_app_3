3905ec6db2fc190df2ee9d2b3c68f760e2fd70f7
João Lucas <jlucasps@gmail.com>
2014-04-04 21:24:46 -0300

    Tela de edição

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find params[:id]
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update(user_params) 
+      render :show
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
